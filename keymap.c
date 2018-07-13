ccf4c136b67015b924fafa6d42d5bf032a93ee8b
Arthur Barton <arthurb@igniferous.net>
2018-07-13 12:17:14 +1000

    Fix the layer swap

--- keymap.c
+++ keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         TO(1),KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                         TO(0),KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
        KC_MPRV, KC_MNXT,
        KC_VOLU,
        KC_VOLD, KC_MUTE, KC_MPLY
