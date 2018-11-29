443a347eb64483131111baf5fa643a845106064b
Arthur Barton <arthurb@igniferous.net>
2018-11-30 07:29:17 +1100

    Slower tapping term

---
+++ config-ez.h
@@ -0,0 +1,9 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+
+#undef RGBLIGHT_BRI_STEP
+#define RGBLIGHT_BRI_STEP 10
